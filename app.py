650771b86b9799ffb04460178ec5dc8411b6d55b
Farhad Hossain <farhadhossain0085@gmail.com>
2020-07-16 22:44:57 +0600

    uploaded file with ajax

--- app.py
+++ app.py
@@ -13,12 +13,16 @@ def index():
         csv_2 = files['file_2']
         csv_3 = files['file_3']
 
+        print(csv_1.filename)
+        print(csv_2.filename)
+        print(csv_3.filename)
+
         if csv_1.filename == "" or csv_2.filename == "" or csv_3.filename == "":
         	return jsonify({"message": "Something is wrong"}), 403
 
         #print(csv_3.read())
 
-    return render_template("index.html", title="Home")
+    return render_template("index.html", title="Home"), 200
 
 @app.route("/result")
 def result():
