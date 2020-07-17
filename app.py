923939b1579fcab6a721df1ef65a70df67bdd795
Farhad Hossain <farhadhossain0085@gmail.com>
2020-07-17 20:37:53 +0600

    filesize calculated

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
         print(csv_3.filename)
 
         if csv_1.filename == "" or csv_2.filename == "" or csv_3.filename == "":
-        	return jsonify({"message": "Something is wrong"}), 401
+            return jsonify({"message": "Something is wrong"}), 401
 
         #print(csv_3.read())
         #time.sleep(1)
@@ -31,15 +31,21 @@ def index():
         csv_3 = io.BytesIO(csv_3.read())
 
         try:
-        	output_csv_1, output_csv_2 = csv_processing(csv_1, csv_2, csv_3)
-        	output_csv_1.to_csv(os.path.join(app.root_path, 'static/csv', "NLGdata.csv"))
-        	output_csv_2.to_csv(os.path.join(app.root_path, 'static/csv', "Chartsdata.csv"))
+            output_csv_1, output_csv_2 = csv_processing(csv_1, csv_2, csv_3)
+            output_csv_1.to_csv(os.path.join(app.root_path, 'static/csv', "NLGdata.csv"))
+            output_csv_2.to_csv(os.path.join(app.root_path, 'static/csv', "Chartsdata.csv"))
+            
+            filesize_1 = os.path.getsize(os.path.join(app.root_path, 'static/csv', "NLGdata.csv"))
+            filesize_2 = os.path.getsize(os.path.join(app.root_path, 'static/csv', "Chartsdata.csv"))
+
+            filesize_1 = str(round(filesize_1/1024, 2)) + " KB"
+            filesize_2 = str(round(filesize_2/1024, 2)) + " KB"
         except:
-        	return jsonify({"message" : "error"}), 401
+            return jsonify({"message" : "error"}), 401
 
         filenames = ["NLGdata.csv", "Chartsdata.csv"]
         filelinks = [url_for('static', filename='csv/NLGdata.csv'), url_for('static', filename='csv/Chartsdata.csv'),]
-        filesize = ["20 MB", "25 MB"]
+        filesize = [filesize_1, filesize_2]
 
         return jsonify({"filenames": filenames, "filesize": filesize, "filelinks": filelinks}), 200
 
